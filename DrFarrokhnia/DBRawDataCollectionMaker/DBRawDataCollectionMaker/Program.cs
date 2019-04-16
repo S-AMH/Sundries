b9cad63b880d1df0bb3507a0993a8a575a57bd81
S.AmirMohammadHasanli <hasnali.amir@gmail.com>
2019-04-16 13:35:53 +0430

    RawDBCollectionMaker Added.

---
+++ DrFarrokhnia/DBRawDataCollectionMaker/DBRawDataCollectionMaker/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleLogger;
+using System.IO;
+using ATG;
+
+namespace DBRawDataCollectionMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DB = @"C:\FWS\DB\rawData\GFS\0.13";
+            DateTime startDate;
+            DateTime endDate;
+            DateTime currentDate;
+            string start;
+            string end;
+            string filePath;
+            string var;
+            List<string> variables = new List<string>();
+
+            SimpleLog.SetLogFile(logDir: ".//Logs", writeText: false);
+            SimpleLog.Info("Program Started.");
+            Console.WriteLine("Please type start date: (yyyy/MM/dd)");
+            start = Console.ReadLine();
+
+            SimpleLog.Info("start Date Entered is: " + start);
+
+            Console.WriteLine("Please type end date: (yyyy/MM/dd)");
+            end = Console.ReadLine();
+
+            SimpleLog.Info("end date entered is: " + end);
+
+            Console.WriteLine("Please enter filePath to save data(press enter to save to current directory)");
+            filePath = Console.ReadLine();
+            Console.WriteLine("Please enter variables sperated with \",\". \n" +
+                "Avaliables are: APCP, APCP-12hr, APCP-24hr, CumulicativeRaster3hr \n" +
+                " CumulicativeRaster3hrAPCP, Daily-APCP, Daily-RAIN, RAIN, RAIN-12hr, RAIN-24hr, SNOW, SOIL, TEMP");
+            var = Console.ReadLine();
+
+            SimpleLog.Info("requested variables are: " + var);
+
+            if (filePath == "/n")
+                filePath = Directory.GetCurrentDirectory();
+
+            SimpleLog.Info("save directory is: " + filePath);
+
+            startDate = new DateTime(
+                Convert.ToInt32(start.Split('/')[0]),
+                Convert.ToInt32(start.Split('/')[1]),
+                Convert.ToInt32(start.Split('/')[2]));
+            endDate = new DateTime(
+                Convert.ToInt32(end.Split('/')[0]),
+                Convert.ToInt32(end.Split('/')[1]),
+                Convert.ToInt32(end.Split('/')[2]));
+
+            variables = var.Split(',').ToList();
+
+            currentDate = startDate;
+
+            while(currentDate <= endDate)
+            {
+                for(int i = 0; i <= variables.Count; i ++)
+                {
+                    string[] run = { "00", "06", "12", "18" };
+                    foreach (var r in run)
+                    {
+                        try
+                        {
+                            atgMethods.Copy(
+                                Path.Combine(DB, currentDate.Year.ToString(),
+                                atgMethods.xDigitNum(currentDate.Month, 2),
+                                atgMethods.xDigitNum(currentDate.Day, 2), r, "3hr", variables[i]),
+                                Path.Combine(filePath, currentDate.Year.ToString(),
+                                atgMethods.xDigitNum(currentDate.Month, 2),
+                                atgMethods.xDigitNum(currentDate.Day, 2), r, "3hr", variables[i])
+                                );
+
+                            SimpleLog.Info(Path.Combine(DB, currentDate.Year.ToString(),
+                                atgMethods.xDigitNum(currentDate.Month, 2),
+                                atgMethods.xDigitNum(currentDate.Day, 2), r, "3hr", variables[i])
+                                + " Copied successfully.");
+                        }
+                        catch(Exception e)
+                        {
+                            SimpleLog.Error("Follwoing error Occured." + e.Message);
+                            Console.WriteLine("Follwoing error Occured." + e.Message);
+                        }
+                        
+                    }
+                }
+                currentDate = currentDate.AddDays(1);
+            }
+        }
+    }
+}
---
+++ DrFarrokhnia/DBRawDataCollectionMaker/DBRawDataCollectionMaker/atgMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleLogger;
+using System.IO;
+
+namespace ATG
+{
+    static public class atgMethods
+    {
+        /// <summary>
+        ///  Converts an integer to string with fixed digit count by adding zeros to left.
+        /// </summary>
+        /// <param name="_number">Number which should be converted to string with fixed digit count.</param>
+        /// <param name="_digitsCount">Digit Counts for Corresponding integer.</param>
+        /// <returns>String Corresponding to _number integer with _digitCount digits.</returns>
+        /// <exception cref="System.ArgumentException">Thrown When <c>_number</c> digit counts is lower than <c>_digitsCount</c></exception>
+        static public string xDigitNum(int _number, int _digitsCount)
+        {
+            if (_number.ToString().Length > _digitsCount)
+                throw new ArgumentException("Error: Operation is not valid. digitCounts is lower than number's length.", "_digitCount");
+
+            string result = "";
+            for (int i = 0; i < _digitsCount - _number.ToString().Length; i++)
+                result += "0";
+            return result + _number.ToString();
+        }
+        static public void Copy(string sourceDirectory, string targetDirectory)
+        {
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
